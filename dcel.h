ab3c89ee7d415caaded73b54be682130cb6c43c2
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-07-25 22:50:52 +0300

    dcel.h

--- dcel.h
+++ dcel.h
@@ -65,7 +65,6 @@ private:
 };
 
 std::vector<Vertex> generateRandomDOTS(int n, int width, int height);
-Vertex we_should_find_centre(const Vertex& a, const Vertex& b, const Vertex& c);
 double det3x3(double m00, double m01, double m02,
               double m10, double m11, double m12,
               double m20, double m21, double m22);
